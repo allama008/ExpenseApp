e7a63ef7f7f37c8aeeae9cbee3976ed663cb0663
Allama Hossain <a2hossain2000@yahoo.com>
2021-06-09 00:28:41 +0530

    L.149 Understanding the Widget Lifecycle.

--- widgets/new_transaction.dart
+++ widgets/new_transaction.dart
@@ -6,16 +6,43 @@ import 'package:expenses_app/widgets/adaptive_flat_button.dart';
 
 class NewTransaction extends StatefulWidget {
   final Function changeHandler;
-  NewTransaction(this.changeHandler);
+  NewTransaction(this.changeHandler) {
+    print('Constructor NewTransaction Widget');
+  }
 
   @override
-  _NewTransactionState createState() => _NewTransactionState();
+  _NewTransactionState createState() {
+    print('createState NewTransaction Widget');
+    return _NewTransactionState();
+  }
 }
 
 class _NewTransactionState extends State<NewTransaction> {
   final _titleController = TextEditingController();
   final _amountController = TextEditingController();
   DateTime _selectedDate;
+
+  _NewTransactionState() {
+    print('Constructor NewTransaction State');
+  }
+
+  @override
+  void initState() {
+    print('initState()');
+    super.initState();
+  }
+
+  @override
+  void didUpdateWidget(covariant NewTransaction oldWidget) {
+    print('didUpdateWidget()');
+    super.didUpdateWidget(oldWidget);
+  }
+
+  @override
+  void dispose() {
+    print('dispose()');
+    super.dispose();
+  }
 
   void _submitData() {
     if (_amountController == null) return;
